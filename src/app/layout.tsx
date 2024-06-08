46b08c9f04663ba586d034b1c0b8504f9a1250cc
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-08 21:33:52 +0300

    added AptosProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
+import AptosProvider from '@/context/AptosProvider';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'], weight: ['400', '500', '600'] });
@@ -16,7 +19,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
-            <body className={inter.className}>{children}</body>
+            <AptosProvider>
+                <body className={inter.className}>{children}</body>
+            </AptosProvider>
         </html>
     );
 }
