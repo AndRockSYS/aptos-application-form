f8f57a931da83473c0ba00f58c01091b58d25f58
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-08 17:25:35 +0300

    added types

---
+++ typings.ts
@@ -0,0 +1,28 @@
+export enum BusinessType {
+    Trade,
+    Services,
+}
+
+export interface Company {
+    name: string;
+    registrationNumber: string;
+    country: string;
+    type: BusinessType;
+}
+
+export interface Address {
+    street: string;
+    street2?: string;
+    city: string;
+    state: string;
+    postalCode: string;
+}
+
+export interface ApplicationForm {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    company: Company;
+    address: Address;
+}
