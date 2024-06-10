4086d3ce5e0283ef99e5ca180d4d2ec8a7314eaa
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-10 22:05:54 +0300

    added owner-pad page

--- src/app/owner-pad/page.tsx
+++ src/app/owner-pad/page.tsx
@@ -2,38 +2,120 @@
 
 import { PetraWalletName } from 'petra-plugin-wallet-adapter';
 import { useWallet } from '@aptos-labs/wallet-adapter-react';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
+import ApprovementForm from '@/components/ApprovementForm';
+
+import KeyStore from '@/class/KeyStore';
+
+import useData from '@/hooks/useData';
 import useAptos from '@/hooks/useAptos';
-import Application from '@/components/Application';
+
+import { ApplicationForm, AptosApplication } from 'typings';
+
+import './owner-pad.css';
 
 export default function OwnerPad() {
     const { account, connect } = useWallet();
-    const { applications, updateApplication } = useAptos();
+    const { applications, approved, updateApplication, updateApproved, reviewApplication } =
+        useAptos();
+    const { decodeData } = useData();
 
-    const buttonMessage = useMemo(() => (account ? 'Retrieve List' : 'Connect Wallet'), [account]);
+    const [isApproved, setIsApproved] = useState(false);
+    const [applicant, setApplicant] = useState('');
+    const [currForm, setCurrForm] = useState<ApplicationForm>({
+        firstName: '',
+        lastName: '',
+        signature: '',
+        email: '',
+        phone: '',
+        company: {
+            name: '',
+            registrationNumber: '',
+            country: '',
+            type: 0,
+        },
+        address: {
+            street: '',
+            city: '',
+            state: '',
+            postalCode: '',
+        },
+    });
 
-    const list = useMemo(
-        () => applications.map((application) => <Application application={application} />),
-        [applications, account]
-    );
+    const openForm = async (applicant: string) => {
+        const keyStore = new KeyStore();
+
+        const privateKey = await keyStore.getKey(applicant);
+
+        const application = (isApproved ? approved : applications).find((item) => {
+            if (item.applicant == applicant) return item;
+        });
+
+        if (!application) return;
+
+        const decodedApplication = decodeData(application.hashData, privateKey);
+
+        if (!decodedApplication) return;
+
+        setApplicant(applicant);
+        setCurrForm(decodedApplication);
+
+        const form = document.querySelector('div.transparent-background') as HTMLElement;
+        form.style.display = 'block';
+    };
+
+    const getList = (array: AptosApplication[], isApproved: boolean) =>
+        array.map((application) => (
+            <div
+                key={application.applicant}
+                className='application'
+                onClick={() => {
+                    setIsApproved(isApproved);
+                    openForm(application.applicant);
+                }}
+            >
+                <h2>
+                    {application.applicant.slice(0, 10) +
+                        '...' +
+                        application.applicant.slice(56, 66)}
+                </h2>
+            </div>
+        ));
 
     return (
         <section className='owner-pad'>
-            {list}
+            <h1>Approved Applications</h1>
+            {useMemo(() => getList(approved, true), [account, approved])}
+            <h1>Applications</h1>
+            {useMemo(() => getList(applications, false), [account, applications])}
             <button
+                id='green-button'
                 type='button'
                 onClick={() => {
                     if (!account) {
-                        if ('aptos' in window) connect(PetraWalletName);
-                        else window.open('https://petra.app/', `_blank`);
+                        'aptos' in window
+                            ? connect(PetraWalletName)
+                            : window.open('https://petra.app/', `_blank`);
                     } else {
                         updateApplication();
+                        updateApproved();
                     }
                 }}
             >
-                {buttonMessage}
+                {useMemo(() => (account ? 'Retrieve List' : 'Connect Wallet'), [account])}
             </button>
+            {useMemo(
+                () => (
+                    <ApprovementForm
+                        applicant={applicant}
+                        isApproved={isApproved}
+                        form={currForm}
+                        reviewApplication={reviewApplication}
+                    />
+                ),
+                [applicant, currForm, isApproved]
+            )}
         </section>
     );
 }
