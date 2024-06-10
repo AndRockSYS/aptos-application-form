df3760eb9fcd454af8f07b2a2e43e24c03135d74
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-10 22:06:15 +0300

    added approvement form to approve/decline forms

---
+++ src/components/ApprovementForm.tsx
@@ -0,0 +1,88 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import { ApplicationForm, BusinessType } from 'typings';
+
+import './approvement-form.css';
+
+interface Props {
+    applicant: string;
+    isApproved: boolean;
+    form: ApplicationForm;
+    reviewApplication: (applicant: string, isApproved: boolean) => Promise<void>;
+}
+
+export default function ApprovementForm({ applicant, form, isApproved, reviewApplication }: Props) {
+    const { company, address } = form;
+
+    const closeForm = () => {
+        const form = document.querySelector('div.transparent-background') as HTMLElement;
+        form.style.display = 'none';
+    };
+
+    useEffect(() => {
+        const canvas = document.querySelector(
+            'section.approvement-form canvas'
+        ) as HTMLCanvasElement;
+        const context = canvas.getContext('2d');
+
+        const image = new Image(200);
+        image.src = form.signature;
+        context?.drawImage(image, 0, 0);
+    }, [form]);
+
+    return (
+        <div className='transparent-background'>
+            <section className='approvement-form'>
+                <button id='green-button' onClick={closeForm}>
+                    Close
+                </button>
+                <article className='company'>
+                    <h2>Company</h2>
+                    <h3>Name: {company.name}</h3>
+                    <h3>Registration Number: {company.registrationNumber}</h3>
+                    <h3>Country: {company.country}</h3>
+                    <h3>
+                        Business Type:
+                        {company.type == BusinessType.Services ? 'Services' : 'Trade'}
+                    </h3>
+                </article>
+                <article className='address'>
+                    <h2>Business Address</h2>
+                    <h3>Street Address: {address.street}</h3>
+                    <h3>Street Address Line 2: {address.street2 ? address.street2 : 'None'}</h3>
+                    <h3>City: {address.city}</h3>
+                    <h3>State / Province: {address.state}</h3>
+                    <h3>Postal / Zip Code: {address.postalCode}</h3>
+                </article>
+                <article className='contact'>
+                    <h2>Contact Information</h2>
+                    <h3>Email: {form.email}</h3>
+                    <h3>Phone Number: {form.phone}</h3>
+                </article>
+                <article>
+                    <h2>Authorised Personnel</h2>
+                    <h3>First Name: {form.firstName}</h3>
+                    <h3>Last Name: {form.lastName}</h3>
+                    <canvas></canvas>
+                </article>
+                {isApproved ? (
+                    <></>
+                ) : (
+                    <div>
+                        <button
+                            id='green-button'
+                            onClick={() => reviewApplication(applicant, true)}
+                        >
+                            Approve
+                        </button>
+                        <button id='red-button' onClick={() => reviewApplication(applicant, false)}>
+                            Decline
+                        </button>
+                    </div>
+                )}
+            </section>
+        </div>
+    );
+}
