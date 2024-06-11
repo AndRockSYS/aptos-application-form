1dbf4dfaf85bc911282a55c8bfa702958234851f
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-11 11:07:09 +0300

    added useMemo for buttons

--- src/components/ApprovementForm.tsx
+++ src/components/ApprovementForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import { ApplicationForm, BusinessType } from 'typings';
 
@@ -14,22 +14,39 @@ interface Props {
 }
 
 export default function ApprovementForm({ applicant, form, isApproved, reviewApplication }: Props) {
-    const { company, address } = form;
-
     const closeForm = () => {
         const form = document.querySelector('div.transparent-background') as HTMLElement;
         form.style.display = 'none';
     };
 
-    useEffect(() => {
-        const canvas = document.querySelector(
-            'section.approvement-form canvas'
-        ) as HTMLCanvasElement;
-        const context = canvas.getContext('2d');
+    const buttons = useMemo(
+        () =>
+            isApproved ? (
+                <></>
+            ) : (
+                <div>
+                    <button id='green-button' onClick={() => reviewApplication(applicant, true)}>
+                        Approve
+                    </button>
+                    <button id='red-button' onClick={() => reviewApplication(applicant, false)}>
+                        Decline
+                    </button>
+                </div>
+            ),
+        [isApproved]
+    );
 
-        const image = new Image(200);
-        image.src = form.signature;
-        context?.drawImage(image, 0, 0);
+    useEffect(() => {
+        if (form.signature) {
+            const canvas = document.querySelector(
+                'section.approvement-form canvas'
+            ) as HTMLCanvasElement;
+            const context = canvas.getContext('2d');
+
+            const image = new Image(200);
+            image.src = form.signature;
+            context?.drawImage(image, 0, 0);
+        }
     }, [form]);
 
     return (
@@ -40,21 +57,21 @@ export default function ApprovementForm({ applicant, form, isApproved, reviewApp
                 </button>
                 <article className='company'>
                     <h2>Company</h2>
-                    <h3>Name: {company.name}</h3>
-                    <h3>Registration Number: {company.registrationNumber}</h3>
-                    <h3>Country: {company.country}</h3>
-                    <h3>
-                        Business Type:
-                        {company.type == BusinessType.Services ? 'Services' : 'Trade'}
-                    </h3>
+                    <h3>Name: {form.company.name}</h3>
+                    <h3>Registration Number: {form.company.registrationNumber}</h3>
+                    <h3>Country: {form.company.country}</h3>
+                    <h3>Business Type: </h3>
                 </article>
                 <article className='address'>
                     <h2>Business Address</h2>
-                    <h3>Street Address: {address.street}</h3>
-                    <h3>Street Address Line 2: {address.street2 ? address.street2 : 'None'}</h3>
-                    <h3>City: {address.city}</h3>
-                    <h3>State / Province: {address.state}</h3>
-                    <h3>Postal / Zip Code: {address.postalCode}</h3>
+                    <h3>Street Address: {form.address.street}</h3>
+                    <h3>
+                        Street Address Line 2:{' '}
+                        {form.address.street2 ? form.address.street2 : 'None'}
+                    </h3>
+                    <h3>City: {form.address.city}</h3>
+                    <h3>State / Province: {form.address.state}</h3>
+                    <h3>Postal / Zip Code: {form.address.postalCode}</h3>
                 </article>
                 <article className='contact'>
                     <h2>Contact Information</h2>
@@ -67,21 +84,7 @@ export default function ApprovementForm({ applicant, form, isApproved, reviewApp
                     <h3>Last Name: {form.lastName}</h3>
                     <canvas></canvas>
                 </article>
-                {isApproved ? (
-                    <></>
-                ) : (
-                    <div>
-                        <button
-                            id='green-button'
-                            onClick={() => reviewApplication(applicant, true)}
-                        >
-                            Approve
-                        </button>
-                        <button id='red-button' onClick={() => reviewApplication(applicant, false)}>
-                            Decline
-                        </button>
-                    </div>
-                )}
+                {buttons}
             </section>
         </div>
     );
