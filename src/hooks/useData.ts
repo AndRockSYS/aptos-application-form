35f18da322abc9fd22365cf9834724c18cc56865
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-12 16:13:12 +0300

    logo hashing

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,7 +1,7 @@
 import CryptoJS from 'crypto-js';
 import { InputTransactionData, useWallet } from '@aptos-labs/wallet-adapter-react';
 
-import KeyStore from '@/class/KeyStore';
+import Firebase from '@/service/Firebase';
 
 import { ApplicationForm, BusinessType } from 'typings';
 
@@ -23,59 +23,79 @@ const useData = () => {
         return true;
     };
 
-    const collectData = (signatureData: string): ApplicationForm => {
+    const collectData = async (signatureData: string): Promise<ApplicationForm | undefined> => {
         const inputs = document.querySelectorAll(
             'section.application > form input, section.application > form select'
         ) as NodeListOf<HTMLInputElement>;
 
+        const imageInput = inputs[4].files?.item(0);
+
+        console.log(imageInput?.size);
+
+        if (imageInput && imageInput.size > 5000) {
+            alert('The logo is too big, max is 5kB');
+            return undefined;
+        }
+
+        const image = await inputs[4].files?.item(0)?.arrayBuffer();
+        if (!image) return undefined;
+
+        const buffer = Buffer.from(image);
+
         return {
             company: {
+                logo: buffer.toString('base64'),
                 name: inputs[0].value,
                 registrationNumber: inputs[1].value,
                 country: inputs[2].value,
                 type: inputs[3].value == 'Trade' ? BusinessType.Trade : BusinessType.Services,
             },
             address: {
-                street: inputs[4].value,
-                street2: inputs[5].value,
-                city: inputs[6].value,
-                state: inputs[7].value,
-                postalCode: inputs[8].value,
+                street: inputs[5].value,
+                street2: inputs[6].value,
+                city: inputs[7].value,
+                state: inputs[8].value,
+                postalCode: inputs[9].value,
             },
-            email: inputs[9].value,
-            phone: inputs[10].value,
-            firstName: inputs[11].value,
-            lastName: inputs[12].value,
+            email: inputs[10].value,
+            phone: inputs[11].value,
+            firstName: inputs[12].value,
+            lastName: inputs[13].value,
             signature: signatureData,
         };
     };
 
-    const sendForm = (signatureData: string) => {
+    const sendForm = async (signatureData: string) => {
         if (!account) return;
 
-        const application = collectData(signatureData);
+        const application = await collectData(signatureData);
 
-        hashData(application).then(async ([data, privateKey]) => {
-            const keyStore = new KeyStore();
+        if (!application) {
+            alert(`Could not get applicaton data`);
+            return;
+        }
 
-            await keyStore.addKey(account.address, privateKey).catch((error) => {
-                alert(`Error - ${error}`);
-                return;
-            });
+        const [data, privateKey] = await hashData(application);
 
-            const transaction: InputTransactionData = {
-                data: {
-                    function: `${
-                        process.env.NEXT_PUBLIC_MODULE_ADDRESS as string
-                    }::application_form::add_application`,
-                    functionArguments: [data],
-                },
-            };
+        const firebase = new Firebase();
 
-            await signAndSubmitTransaction(transaction).catch((error) =>
-                alert(`Error occured - ${error}`)
-            );
+        await firebase.addApplicant(account.address, privateKey).catch((error) => {
+            alert(error);
+            return;
         });
+
+        const transaction: InputTransactionData = {
+            data: {
+                function: `${
+                    process.env.NEXT_PUBLIC_MODULE_ADDRESS as string
+                }::application_form::add_application`,
+                functionArguments: [data],
+            },
+        };
+
+        await signAndSubmitTransaction(transaction).catch((error) =>
+            alert(`Error occured - ${error}`)
+        );
     };
 
     const hashData = async (form: ApplicationForm): Promise<string[]> => {
